9e7415bf7eeefa135cffc47e23e0e95fa28ffa5f
Anuj Taxali <anujtaxali@gmail.com>
2018-02-28 10:30:20 -0500

    Deck will now shuffle when page loaded

--- js/app.js
+++ js/app.js
@@ -1,35 +1,16 @@
-/*
- * Create a list that holds all of your cards
- */
+function shuffle() {
+	var ul = document.getElementById("deck");
+	for (var i = ul.children.length; i >= 0; i--) {
+	    ul.appendChild(ul.children[Math.random() * i | 0]);
+	}
+}
 
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-};
+shuffle();
 
 const newList = document.createElement('UL');
 const deck = document.getElementById('deck');
 
 deck.addEventListener('click', function(ev) {
-
 	document.getElementById("moves").textContent = (parseInt(document.getElementById("moves").textContent) + 1).toString();
 
 	if (newList.childElementCount<2) {
